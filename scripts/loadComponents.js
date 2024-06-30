fde5c78f88eadce8bf8a9e6987d0575a1d14a20c
Shiloh Rodrigues <shilohrodrigues@hotmail.com>
2024-06-30 15:24:16 -0400

    Fixing path issue

--- scripts/loadComponents.js
+++ scripts/loadComponents.js
@@ -1,28 +1,26 @@
 function loadContent() {
-const basePath = window.location.pathname.includes('Enerzam/') ? '/Enerzam/' : '../';
-console.log(window.location.pathname);
-  fetch(basePath + 'components/header.html')
+  fetch('/components/header.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('header').innerHTML = data;
           // Load header-specific CSS
           const link = document.createElement('link');
           link.rel = 'stylesheet';
-          link.href = basePath + 'assets/styles/header.css';
+          link.href = '/assets/styles/header.css';
           document.head.appendChild(link);
           // Load header-specific JavaScript
           const script = document.createElement('script');
-          script.src = basePath + 'scripts/header.js';
+          script.src = '/scripts/header.js';
           document.body.appendChild(script);
       });
-  fetch(basePath + 'components/footer.html')
+  fetch('/components/footer.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('footer').innerHTML = data;
           // Load header-specific CSS
           const link = document.createElement('link');
           link.rel = 'stylesheet';
-          link.href = basePath + 'assets/styles/footer.css';
+          link.href = '/assets/styles/footer.css';
           document.head.appendChild(link);
       });
 }
