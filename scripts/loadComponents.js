1bc4978bf2fcb5371a4d2d4ce14dffbc65a34232
Shiloh Rodrigues <shilohrodrigues@hotmail.com>
2024-06-30 11:34:39 -0400

    Fixed html paths

--- scripts/loadComponents.js
+++ scripts/loadComponents.js
@@ -1,26 +1,26 @@
 function loadContent() {
-  fetch('./components/header.html')
+  fetch('/components/header.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('header').innerHTML = data;
           // Load header-specific CSS
           const link = document.createElement('link');
           link.rel = 'stylesheet';
-          link.href = './assets/styles/header.css';
+          link.href = '/assets/styles/header.css';
           document.head.appendChild(link);
           // Load header-specific JavaScript
           const script = document.createElement('script');
-          script.src = './scripts/header.js';
+          script.src = '/scripts/header.js';
           document.body.appendChild(script);
       });
-  fetch('./components/footer.html')
+  fetch('/components/footer.html')
       .then(response => response.text())
       .then(data => {
           document.getElementById('footer').innerHTML = data;
           // Load header-specific CSS
           const link = document.createElement('link');
           link.rel = 'stylesheet';
-          link.href = './assets/styles/footer.css';
+          link.href = '/assets/styles/footer.css';
           document.head.appendChild(link);
       });
 }
